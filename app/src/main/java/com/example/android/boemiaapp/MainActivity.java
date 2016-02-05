69f7d7223722ea0f15797fe8b8d0e355bf310a32
gjezzi <gjezzi@gmail.com>
2016-02-05 15:15:14 -0200

    Toolbar Layout - Buttons

--- app/src/main/java/com/example/android/boemiaapp/MainActivity.java
+++ app/src/main/java/com/example/android/boemiaapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.boemiaapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
