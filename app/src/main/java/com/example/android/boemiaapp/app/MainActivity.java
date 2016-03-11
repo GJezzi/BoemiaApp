fae6041def8be04bc792da52a168ce7583bbf634
gjezzi <gjezzi@gmail.com>
2016-03-11 13:44:16 -0300

    FB Login Implementation - Refactor

--- app/src/main/java/com/example/android/boemiaapp/app/MainActivity.java
+++ app/src/main/java/com/example/android/boemiaapp/app/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.android.boemiaapp.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 import com.example.android.boemiaapp.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton mFbLoginBtn;
+    private CallbackManager mCallBackManager;
+    private TextView mUserName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        mUserName = (TextView) findViewById(R.id.user_name);
+        mFbLoginBtn = (LoginButton) findViewById(R.id.fb_login_button);
+        mFbLoginBtn.setReadPermissions("user_friends");
+
+        mCallBackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode,data);
     }
 }
