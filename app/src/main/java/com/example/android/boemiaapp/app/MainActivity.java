95ed0a190a6615d61ea3de821bc9f027dcf25ada
gjezzi <gjezzi@gmail.com>
2016-03-15 11:22:30 -0300

    FB Login - User Name - Finished

--- app/src/main/java/com/example/android/boemiaapp/app/MainActivity.java
+++ app/src/main/java/com/example/android/boemiaapp/app/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.android.boemiaapp.app;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import com.example.android.boemiaapp.R;
 import com.facebook.FacebookSdk;
@@ -11,29 +9,14 @@ import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentManager mFragmentManager;
+    public static final String FRAGMENT_TAG = "fragment_tag";
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFragmentManager = getSupportFragmentManager();
-    }
-
-
-    public void onSavedInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-
-    public boolean onCreatedOptionsMenu(MenuItem menuItem) {
-        int id = menuItem.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(menuItem);
     }
 }
--- app/src/main/java/com/example/android/boemiaapp/app/MainFragment.java
+++ app/src/main/java/com/example/android/boemiaapp/app/MainFragment.java
@@ -11,16 +11,20 @@ import android.widget.TextView;
 
 import com.example.android.boemiaapp.R;
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class MainFragment extends Fragment {
 
     private LoginButton mFbLoginButton;
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
     private CallbackManager mCallbackManager;
     private TextView mUserName;
     private FacebookCallback<LoginResult> mFbCallback = new FacebookCallback<LoginResult>() {
@@ -29,10 +33,7 @@ public class MainFragment extends Fragment {
             Log.d("Gian", "onSucces");
             AccessToken accessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
-
-            if (profile != null) {
-                mUserName.setText("Bem Vindo " + profile.getName());
-            }
+            mUserName.setText(setupWelcomeMessage(profile));
         }
 
         @Override
@@ -52,6 +53,12 @@ public class MainFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         mCallbackManager = CallbackManager.Factory.create();
+
+        setupTokenTracker();
+        setupProfileTracker();
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
     }
 
     @Override
@@ -63,17 +70,66 @@ public class MainFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mFbLoginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
-        mFbLoginButton.setReadPermissions("user_friends");
-        mFbLoginButton.setFragment(this);
-        mFbLoginButton.registerCallback(mCallbackManager, mFbCallback);
+        setupUserDetails(view);
+        setupFbLoginButton(view);
 
-        mUserName = (TextView) view.findViewById(R.id.user_name);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        mUserName.setText(setupWelcomeMessage(profile));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.stopTracking();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setupUserDetails(View view){
+        mUserName = (TextView) view.findViewById(R.id.user_name);
+    }
+
+    private void setupTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.d("Current Token: ", "" + currentAccessToken);
+            }
+        };
+    }
+
+    private void setupProfileTracker() {
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.d("Profile: ", "" + currentProfile);
+                mUserName.setText(setupWelcomeMessage(currentProfile));
+            }
+        };
+    }
+
+    private void setupFbLoginButton(View view) {
+        mFbLoginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
+        mFbLoginButton.setFragment(this);
+        mFbLoginButton.setReadPermissions("user_friends");
+        mFbLoginButton.registerCallback(mCallbackManager, mFbCallback);
+    }
+
+    private String setupWelcomeMessage(Profile profile) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (profile != null) {
+            stringBuilder.append("Welcome " + profile.getName() + "!");
+        }
+        return stringBuilder.toString();
     }
 }
