3b1fca0c9189cfd62965ca6545ad8755e56872ac
gjezzi <gjezzi@gmail.com>
2016-03-30 12:35:23 -0300

    Place Picker Implementation

--- app/src/main/java/com/example/android/boemiaapp/app/LocationActivity.java
+++ app/src/main/java/com/example/android/boemiaapp/app/LocationActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.android.boemiaapp.R;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.ArrayList;
@@ -13,19 +15,20 @@ import java.util.ArrayList;
 public class LocationActivity extends AppCompatActivity {
 
     private static String LOG_TAG = LocationActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-
-    private GoogleApiAvailability mGoogleApiAvailability;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        checkPlayServices();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.location_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -49,5 +52,27 @@ public class LocationActivity extends AppCompatActivity {
                     .add(R.id.location_container, locationFragment)
                     .commit();
         }
+
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(LOG_TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/android/boemiaapp/app/LocationFragment.java
+++ app/src/main/java/com/example/android/boemiaapp/app/LocationFragment.java
@@ -1,25 +1,56 @@
 package com.example.android.boemiaapp.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android.boemiaapp.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by gjezzi on 18/03/16.
  */
 public class LocationFragment extends Fragment {
 
-    public static final String LOG_TAG = LocationFragment.class.getSimpleName();
+    private final static int PLACE_PICKER_REQUEST = 9191;
+
+    private FloatingActionButton mFAB;
 
     public LocationFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_location, container, false);
+
+        mFAB = (FloatingActionButton) rootView.findViewById(R.id.location_fab);
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                    Intent intent = builder.build(getActivity());
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                }
+                catch (GooglePlayServicesRepairableException e) {
+                    GooglePlayServicesUtil.showErrorDialogFragment(e.getConnectionStatusCode(), getActivity(), 0);
+
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    Toast.makeText(getActivity(), "Google Play Services is not Available", Toast.LENGTH_LONG).show();
+                }
+
+
+
+            }
+        });
 
         return rootView;
     }
